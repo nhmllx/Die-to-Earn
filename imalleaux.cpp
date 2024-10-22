47a76260f7ca8ab53f3a03dedbb029d6b1da73eb
Isaiah Malleaux <imalleaux@odin.cs.csubak.edu>
2024-10-22 08:41:13 -0700

    include guard

--- imalleaux.cpp
+++ imalleaux.cpp
@@ -13,8 +13,10 @@
 
 * History: Written by Isaiah Malleaux, 9/24.
 */
+#ifndef IMALLEAUX_H
+#define IMALLEAUX_H
 
-//added wastelands.jpg
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,43 +27,7 @@
 #include <X11/keysym.h>
 #include <GL/glx.h>
 #include "fonts.h"
-//Image img[3] = {"images/walk.gif", "images/car.png", "images/wastelands.jpg"};
-//new class for main code
-/*class Texture {
-    public:
-        Image *backImage;
-        GLuint backTexture;
-        float xc[2];
-        float yc[2];
-}; 
-// In class Global
-int flag
-Texture tex;
-// In Globals constructor
-flag = 1;
-// new render
-*/
-// I added something in main, but cannot show :(
 
-//in void initOpengl(void)
-/*    g.tex.backImage = &img[2];
-    //create opengl texture elements
-    glGenTextures(1, &g.tex.backTexture);
-    int ww = g.tex.backImage->width;
-    int hh = g.tex.backImage->height;
-    glBindTexture(GL_TEXTURE_2D, g.tex.backTexture);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, ww, hh, 0,
-            GL_RGB, GL_UNSIGNED_BYTE, g.tex.backImage->data);
-    g.tex.xc[0] = 0.0;
-    g.tex.xc[1] = 0.25;
-    g.tex.yc[0] = 0.0;
-    g.tex.yc[1] = 1.0;
-*/
-//in int checkKeys(XEvent *e)
-//case XK_Up:
-//    g.flag = 0;
 void scroll(float val[])
 {
     val[0] += 0.001;
@@ -84,6 +50,6 @@ void render2(float x[], float y[], GLuint bt, int xres,int yres)
     //draw background
 }
                        
-
+#endif
 
 
