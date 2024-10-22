f7cf19bc6e06d52216e1ad32327ef1c9f7e2d15f
Noah <128880146+nhmllx@users.noreply.github.com>
2024-10-22 09:52:27 -0700

    Update nmalleaux.cpp

--- nmalleaux.cpp
+++ nmalleaux.cpp
@@ -1,7 +1,19 @@
-//Noah Malleaux
-//
-//Scource file: gameplay 
-//
+#ifndef NMALLEAUX_H
+#define NMALLEAUX_H
 
-#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+
+
+                       
+#endif
 
