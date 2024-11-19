024a155192d80c16a5f9295196ec63ab6d36d7a9
Noah <128880146+nhmllx@users.noreply.github.com>
2024-11-19 07:20:31 -0800

    Add files via upload
    enemy fixes

--- nmalleaux.cpp
+++ nmalleaux.cpp
@@ -55,81 +55,66 @@ class Enemy {
             pos[1] = 800 / 2; // enemy y position
             vel[0] = 0.0f;
             vel[1] = 0.0f;
-            w = 100;
-            h = 100;
-
+            w = 20;
+            h = 20;
         }
-
 } enemies[MAX_ENEMIES];
 
-
 void enemyAnimate(void) //call in walk.cpp main
-  {
-  anim.recordTime(&anim.timeCurrent);
-        double timeSpan = anim.timeDiff(&anim.spriteTime, &anim.timeCurrent);
-        if (timeSpan > delayer) {
-            ++frameno;
-            if (frameno >= 18)
-                frameno -= 18;
-            anim.recordTime(&anim.spriteTime);
-        }
-        //for (int i=0; i<20; i++) {
-          //  g.box[i][0] -= 2.0 * (0.5 / delayer);
-         //   if (g.box[i][0] < -10.0)
-           //     g.box[i][0] += g.xres + 10.0;
-       // }
-  }
-void enemyRender(GLuint etex) 
 {
-
+    anim.recordTime(&anim.timeCurrent);
+    double timeSpan = anim.timeDiff(&anim.spriteTime, &anim.timeCurrent);
+    if (timeSpan > delayer) {
+        ++frameno;
+        if (frameno >= 18)
+            frameno -= 18;
+        anim.recordTime(&anim.spriteTime);
+    }
+}
+void enemyRender(GLuint etex)
+{
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, etex);
     glEnable(GL_ALPHA_TEST);
     glAlphaFunc(GL_GREATER, 0.0f);
-    float ww = 173.0f;
-    float hh = 44.0f;
+
+    float spriteWidth = 900.0f / 9.0f;  // Width of each sprite in the sprite sheet
+    float spriteHeight = 100.0f;        // Height of each sprite (constant)
 
     glColor3f(1.0, 1.0, 1.0);
 
     for (int i = 0; i < MAX_ENEMIES; i++) {
-        /*glPushMatrix();
-          glTranslatef(enemies[i].pos[0], enemies[i].pos[1], 0.0f);
-          glBegin(GL_QUAS);
-          glTexCoord2f(0, 1); glVertex2f(-enemies[i].w, -enemies[i].h);
-          glTexCoord2f(0, 0); glVertex2f(-enemies[i].w,  enemies[i].h);
-          glTexCoord2f(1, 0); glVertex2f( enemies[i].w,  enemies[i].h);
-          glTexCoord2f(1, 1); glVertex2f( enemies[i].w, -enemies[i].h);
-          glEnd();
-          glPopMatrix();*/
         glPushMatrix();
-        // glColor3f(1.0, 1.0, 1.0);
-        // glBindTexture(GL_TEXTURE_2D, g.walkTexture);
-        //
-        // glEnable(GL_ALPHA_TEST);
         glAlphaFunc(GL_GREATER, 0.0f);
-        glColor4ub(255,255,255,255);
+        glColor4ub(255, 255, 255, 255);
 
-        int ix = frameno % 9; // Get the current sprite frame (0, 1, or 2)
+        int ix = frameno % 9;  // Get the current sprite frame (0 to 8)
 
-        // Calculate texture coordinates based on the current frame
-        float tx = (float)(ix * ww) / 519.0f; // Adjust tx based on the frame
-        float ty = 0.0f; // Only one row, so ty is always 0
+        float tx = (float)(ix * spriteWidth) / 900.0f;  // x offset for current sprite frame
+        float ty = 0.0f;  // y offset (since all frames are in a single row)
+
+        float txEnd = tx + spriteWidth / 900.0f;  // Right boundary for the current sprite
 
         glBegin(GL_QUADS);
-        glTexCoord2f(tx, ty+1.0f); 
-        glVertex2f(-enemies[i].w - ww, -enemies[i].h - hh); // Top-left
-        glTexCoord2f(tx, ty); 
-        glVertex2f(-enemies[i].w - ww, enemies[i].h + hh); // Bottom-left
-        glTexCoord2f(tx + 1.0f / 3.0f, ty); 
-        glVertex2f(enemies[i].w + ww, enemies[i].h + hh);
-        glTexCoord2f(tx + 1.0f / 3.0f, ty + 1.0f); 
-        glVertex2f(enemies[i].w + ww, -enemies[i].h - hh); // Top-right
+        // Top-left corner
+        glTexCoord2f(tx, ty + 1.0f);
+        glVertex2f(enemies[i].pos[0] - spriteWidth / 2, enemies[i].pos[1] - spriteHeight / 2);
+        // Bottom-left corner
+        glTexCoord2f(tx, ty);
+        glVertex2f(enemies[i].pos[0] - spriteWidth / 2, enemies[i].pos[1] + spriteHeight / 2);
+        // Bottom-right corner
+        glTexCoord2f(txEnd, ty);
+        glVertex2f(enemies[i].pos[0] + spriteWidth / 2, enemies[i].pos[1] + spriteHeight / 2);
+        // Top-right corner
+        glTexCoord2f(txEnd, ty + 1.0f);
+        glVertex2f(enemies[i].pos[0] + spriteWidth / 2, enemies[i].pos[1] - spriteHeight / 2);
         glEnd();
         glPopMatrix();
     }
 
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_ALPHA_TEST);
-}    
+}
 #endif
+
 
